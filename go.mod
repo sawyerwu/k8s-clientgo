767ab0fd53c7fac7af4ed79ea2c48cba826df7c9
sawyerwu <664269713@qq.com>
2021-01-24 22:43:50 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sawyerwu/k8s-clientgo
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	fmt.Println(kubeconfig)
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
+
