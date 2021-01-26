0cffce95528126db2c65d91ca2fe6aaa0fa68fc1
sawyerwu <664269713@qq.com>
2021-01-26 22:02:58 +0800

    client create pod

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/sawyerwu/k8s-clientgo
 
 go 1.15
+
+require (
+	k8s.io/api v0.17.2
+	k8s.io/apimachinery v0.17.2
+	k8s.io/client-go v0.17.2
+)
--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/kubernetes"
 	"os"
 	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
@@ -15,7 +21,65 @@ func main() {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
-	fmt.Println(kubeconfig)
+	fmt.Println(*kubeconfig)
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	/*pods, err := clientset.CoreV1().Pods("application").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name)
+	}*/
+	createPod(clientset, "demo")
+}
+
+func createPod(clientset *kubernetes.Clientset, namespace string) {
+	ns, err := clientset.CoreV1().Namespaces().Get(namespace, metav1.GetOptions{})
+	if err != nil {
+		fmt.Println(ns, "does not exist")
+		panic(err.Error())
+	}
+
+	if errors.IsNotFound(err) {
+		_, err := clientset.CoreV1().Namespaces().Create(&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "demo"}})
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	// create, err := clientset.CoreV1().Namespaces().Create(&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "demo"}})
+
+	pod := getNginxPod()
+	newPod, err := clientset.CoreV1().Pods(namespace).Create(pod)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("pod {} created successfully", newPod)
+}
+
+func getNginxPod() *corev1.Pod {
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "nginx-pod",
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "nginx-c",
+					Image: "nginx:alpine",
+				},
+			},
+		},
+	}
 }
 
 func homeDir() string {
@@ -24,4 +88,3 @@ func homeDir() string {
 	}
 	return os.Getenv("USERPROFILE") // windows
 }
-
